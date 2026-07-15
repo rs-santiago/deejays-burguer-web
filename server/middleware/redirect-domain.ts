c56cbf89cb328d73a544c43e3dd75012d87f9899
rs-santiago <rs.santiago1997@gmail.com>
2026-07-15 15:41:33 -0300

    feat: add logging for host and path in redirect middleware

--- server/middleware/redirect-domain.ts
+++ server/middleware/redirect-domain.ts
@@ -2,6 +2,8 @@ export default defineEventHandler((event) => {
   const host = getRequestHeader(event, 'host')
   const path = event.path
 
+  console.log(`Host: ${host}, Path: ${path}`);
+
   // Substitua pelo domínio que deve ser redirecionado
   const targetDomain = 'https://www.deejaysburguer.com.br'
 
